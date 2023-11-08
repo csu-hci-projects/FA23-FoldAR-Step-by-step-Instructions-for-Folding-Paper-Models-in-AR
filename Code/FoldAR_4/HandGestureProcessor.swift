755922e1533271470853786951a80c6bb9966224
Tani Cath <tanicath@gmail.com>
2023-11-08 13:25:52 -0700

    Logging overhaul
    - Added headers constant
    - Reformatted to data log
    - Moved file checking to separate function
    - Moved file writing to separate function
    - Added data parsing function
    - Modified processPointsPair to take both typedef'd PointsPair and array of CGPoints

--- Code/FoldAR_4/HandGestureProcessor.swift
+++ Code/FoldAR_4/HandGestureProcessor.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class HandGestureProcessor
 {
+    // CONSTANTS
     var startCollection = false
     var savedName = "none"
     var switchState = 0
     private var frameCounter = 0
     private let writeInterval = 5
+    
+    
+    private let headers = "pid, mode, frame, timestamp, thumbTip.x, thumbTip.y, thumbIP.x, thumbIP.y, thumbMP.x, thumbMP.y, thumbCMC.x, thumbCMC.y, thumbTip2.x, thumbTip2.y, thumbIP2.x, thumbIP2.y, thumbMP2.x, thumbMP2.y, thumbCMC2.x, thumbCMC2.y, indexTip.x, indexTip.y, indexPIP.x, indexPIP.y, indexDIP.x, indexDIP.y, indexMCP.x, indexMCP.y, indexTip2.x, indexTip2.y, indexPIP2.x, indexPIP2.y, indexDIP2.x, indexDIP2.y, indexMCP2.x, indexMCP2.y, middleTip.x, middleTip.y, middlePIP.x, middlePIP.y, middleDIP.x, middleDIP.y, middleMCP.x, middleMCP.y, middleTip2.x, middleTip2.y, middlePIP2.x, middlePIP2.y, middleDIP2.x, middleDIP2.y, middleMCP2.x, middleMCP2.y, ringTip.x, ringTip.y, ringPIP.x, ringPIP.y, ringDIP.x, ringDIP.y, ringMCP.x, ringMCP.y, ringTip2.x, ringTip2.y, ringPIP2.x, ringPIP2.y, ringDIP2.x, ringDIP2.y, ringMCP2.x, ringMCP2.y, littleTip.x, littleTip.y, littlePIP.x, littlePIP.y, littleDIP.x, littleDIP.y, littleMCP.x, littleMCP.y, littleTip2.x, littleTip2.y, littlePIP2.x, littlePIP2.y, littleDIP2.x, littleDIP2.y, littleMCP2.x, littleMCP2.y"
+    
     
     enum State
     {
@@ -33,6 +38,8 @@ class HandGestureProcessor
                             littleTip: CGPoint, littlePIP: CGPoint, littleDIP: CGPoint, littleMCP: CGPoint,
                             littleTip2: CGPoint, littlePIP2: CGPoint, littleDIP2: CGPoint, littleMCP2: CGPoint)
     
+//    typealias PointsPair = [CGPoint]
+    
     private var state = State.unknown {
         didSet {
             didChangeStateClosure?(state)
@@ -51,10 +58,12 @@ class HandGestureProcessor
                                                            .zero, .zero, .zero, .zero,
                                                            .zero, .zero, .zero, .zero)
     
-    func processPointsPair(_ pointsPair: PointsPair)
+    func processPointsPair(_ pointsPair: [CGPoint], pp:PointsPair)
     {
-        lastProcessedPointsPair = pointsPair
+        lastProcessedPointsPair = pp
         state = .unknown
+        
+        print(startCollection)
         
         // This stores the captured data in the Documents folder that is accessible via Xcode.
         // Because this app is sandboxed, the only way to get the data is through the following menu:
@@ -68,84 +77,84 @@ class HandGestureProcessor
             frameCounter += 1
             if (frameCounter % writeInterval) == 0
             {
-                if let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
-                {
-                    let fileURL = documentsURL.appendingPathComponent("sessionData5.txt")
-                    
-                    if let outputStream = OutputStream(url: fileURL, append: true)
-                    {
-                        outputStream.open()
-                        var text = ""
-                        
-                        // put timestamp in here!
-                        text += "Name: \(savedName) Mode: \(switchState) "
-                        
-                        let timestamp = Int(Date().timeIntervalSince1970 * 1000)
-                        text += "Timestamp: \(timestamp)\n"
-                        text += "thumbTip.x: \(pointsPair.thumbTip.x), thumbTip.y: \(pointsPair.thumbTip.y)\n"
-                        text += "thumbIP.x: \(pointsPair.thumbIP.x), thumbIP.y: \(pointsPair.thumbIP.y)\n"
-                        text += "thumbMP.x: \(pointsPair.thumbMP.x), thumbMP.y: \(pointsPair.thumbMP.y)\n"
-                        text += "thumbCMC.x: \(pointsPair.thumbBase.x), thumbCMC.y: \(pointsPair.thumbBase.y)\n"
-                        text += "thumbTip2.x: \(pointsPair.thumbTip2.x), thumbTip2.y: \(pointsPair.thumbTip2.y)\n"
-                        text += "thumbIP2.x: \(pointsPair.thumbIP2.x), thumbIP2.y: \(pointsPair.thumbIP2.y)\n"
-                        text += "thumbMP2.x: \(pointsPair.thumbMP2.x), thumbMP2.y: \(pointsPair.thumbMP2.y)\n"
-                        text += "thumbCMC2.x: \(pointsPair.thumbBase2.x), thumbCMC2.y: \(pointsPair.thumbBase2.y)\n"
-                        text += "indexTip.x: \(pointsPair.indexTip.x), indexTip.y: \(pointsPair.indexTip.y)\n"
-                        text += "indexPIP.x: \(pointsPair.indexPIP.x), indexPIP.y: \(pointsPair.indexPIP.y)\n"
-                        text += "indexDIP.x: \(pointsPair.indexDIP.x), indexDIP.y: \(pointsPair.indexDIP.y)\n"
-                        text += "indexMCP.x: \(pointsPair.indexMCP.x), indexMCP.y: \(pointsPair.indexMCP.y)\n"
-                        text += "indexTip2.x: \(pointsPair.indexTip2.x), indexTip2.y: \(pointsPair.indexTip2.y)\n"
-                        text += "indexPIP2.x: \(pointsPair.indexPIP2.x), indexPIP2.y: \(pointsPair.indexPIP2.y)\n"
-                        text += "indexDIP2.x: \(pointsPair.indexDIP2.x), indexDIP2.y: \(pointsPair.indexDIP2.y)\n"
-                        text += "indexMCP2.x: \(pointsPair.indexMCP2.x), indexMCP2.y: \(pointsPair.indexMCP2.y)\n"
-                        text += "middleTip.x: \(pointsPair.middleTip.x), middleTip.y: \(pointsPair.middleTip.y)\n"
-                        text += "middlePIP.x: \(pointsPair.middlePIP.x), middlePIP.y: \(pointsPair.middlePIP.y)\n"
-                        text += "middleDIP.x: \(pointsPair.middleDIP.x), middleDIP.y: \(pointsPair.middleDIP.y)\n"
-                        text += "middleMCP.x: \(pointsPair.middleMCP.x), middleMCP.y: \(pointsPair.middleMCP.y)\n"
-                        text += "middleTip2.x: \(pointsPair.middleTip2.x), middleTip2.y: \(pointsPair.middleTip2.y)\n"
-                        text += "middlePIP2.x: \(pointsPair.middlePIP2.x), middlePIP2.y: \(pointsPair.middlePIP2.y)\n"
-                        text += "middleDIP2.x: \(pointsPair.middleDIP2.x), middleDIP2.y: \(pointsPair.middleDIP2.y)\n"
-                        text += "middleMCP2.x: \(pointsPair.middleMCP2.x), middleMCP2.y: \(pointsPair.middleMCP2.y)\n"
-                        text += "ringTip.x: \(pointsPair.ringTip.x), ringTip.y: \(pointsPair.ringTip.y)\n"
-                        text += "ringPIP.x: \(pointsPair.ringPIP.x), ringPIP.y: \(pointsPair.ringPIP.y)\n"
-                        text += "ringDIP.x: \(pointsPair.ringDIP.x), ringDIP.y: \(pointsPair.ringDIP.y)\n"
-                        text += "ringMCP.x: \(pointsPair.ringMCP.x), ringMCP.y: \(pointsPair.ringMCP.y)\n"
-                        text += "ringTip2.x: \(pointsPair.ringTip2.x), ringTip2.y: \(pointsPair.ringTip2.y)\n"
-                        text += "ringPIP2.x: \(pointsPair.ringPIP2.x), ringPIP2.y: \(pointsPair.ringPIP2.y)\n"
-                        text += "ringDIP2.x: \(pointsPair.ringDIP2.x), ringDIP2.y: \(pointsPair.ringDIP2.y)\n"
-                        text += "ringMCP2.x: \(pointsPair.ringMCP2.x), ringMCP2.y: \(pointsPair.ringMCP2.y)\n"
-                        text += "littleTip.x: \(pointsPair.littleTip.x), littleTip.y: \(pointsPair.littleTip.y)\n"
-                        text += "littlePIP.x: \(pointsPair.littlePIP.x), littlePIP.y: \(pointsPair.littlePIP.y)\n"
-                        text += "littleDIP.x: \(pointsPair.littleDIP.x), littleDIP.y: \(pointsPair.littleDIP.y)\n"
-                        text += "littleMCP.x: \(pointsPair.littleMCP.x), littleMCP.y: \(pointsPair.littleMCP.y)\n"
-                        text += "littleTip2.x: \(pointsPair.littleTip2.x), littleTip2.y: \(pointsPair.littleTip2.y)\n"
-                        text += "littlePIP2.x: \(pointsPair.littlePIP2.x), littlePIP2.y: \(pointsPair.littlePIP2.y)\n"
-                        text += "littleDIP2.x: \(pointsPair.littleDIP2.x), littleDIP2.y: \(pointsPair.littleDIP2.y)\n"
-                        text += "littleMCP2.x: \(pointsPair.littleMCP2.x), littleMCP2.y: \(pointsPair.littleMCP2.y)\n"
-                        text += "END OF DATA FOR FRAME \(frameCounter)\n\n\n"
-                        
-                        let bytesWritten = outputStream.write(text, maxLength: text.count)
-                        
-                        if bytesWritten >= 0
-                        {
-                            print("Wrote \(bytesWritten) bytes to the file.")
-                        } 
-                        else
-                        {
-                            print("Failed to write to the file.")
-                        }
-                        outputStream.close()
-                    } 
-                    else
-                    {
-                        print("Unable to open the file for writing.")
-                    }
-                } 
-                else
-                {
-                    print("Unable to access the Documents directory.")
-                }
+                var text = generateData(savedName: savedName, mode: switchState, frame: frameCounter, pointsPair: pointsPair)
+                // Generate text
+                
+                writeToFile(fileName: "sessionData.csv", data: text, headers: headers)
+                
             }
+        }
+        else{
+            print("stopped")
+            frameCounter = 0
+        }
+    }
+    
+    func generateData(savedName:String, mode: Int, frame:Int, pointsPair: [CGPoint]) -> String{
+        var pID = "none"
+        if savedName != ""{
+            pID = savedName
+        }
+
+        let timestamp = Date.now.formatted()
+        var modeName = ""
+        switch mode {
+        case 0:
+            modeName = "full"
+        case 1:
+            modeName = "hands"
+        case 2:
+            modeName = "blind"
+        default:
+            modeName = ""
+        }
+        var text = "\(pID), \(modeName), \(String(frame)), \(timestamp)"
+        
+        for (index, value) in pointsPair.enumerated(){
+            text += ", \(value.x), \(value.y)"
+        }
+        
+        print(text)
+        return text
+//        text += "\(pointsPair.thumbTip.x), \(pointsPair.thumbTip.y), \(pointsPair.thumbIP.x), ("
+        
+    }
+    
+    func writeToFile(fileName:String, data:String, headers:String){
+        if let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+            let fileURL = documentsURL.appendingPathComponent(fileName)
+            
+            let path = fileURL.path
+            let fm = FileManager.default
+            
+            if !fm.fileExists(atPath: path){
+                print("File does not exist. Creating and writing headers.")
+                writeData(fileURL: fileURL, data: headers, append: false)
+                
+            }else{
+                print("File exists. Appending.")
+                writeData(fileURL: fileURL, data: data, append: true)
+            }
+            
+        }
+    }
+    func writeData(fileURL:URL, data:String, append:Bool){
+        if let outputStream = OutputStream(url: fileURL, append: append){
+            outputStream.open()
+            let bytesWritten = outputStream.write(data, maxLength: data.count)
+            if bytesWritten >= 0
+            {
+                print("\(bytesWritten) bytes written to file.")
+            }
+            else
+            {
+                print("Failed to write to file.")
+            }
+            outputStream.close()
+        }
+        else
+        {
+            print("Unable to open file at \(fileURL) for writing.")
         }
     }
 }
